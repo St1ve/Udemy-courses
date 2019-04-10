4c2aeb584cb8f4807915e294ae28020cb7977689
Victor Razdyakonov laptop <razvical@mail.ru>
2019-04-10 18:30:01 +0300

    jQuery ex1.

---
+++ DOM/js/ex1.js
@@ -0,0 +1,7 @@
+$("div").css("backgroundColor", "purple");
+
+$(".hightlight").css("width", "200px");
+
+$("#third").css("border", "2px solid yellow");
+
+$("div:first-of-type").css("color", "pink");
